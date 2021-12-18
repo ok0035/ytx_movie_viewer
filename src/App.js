816b2cbdb091653e14513b2d16a3b6c8fdd9ace5
youngdo <zerodeg93@gmail.com>
2021-12-19 00:34:17 +0900

    add router basename

--- src/App.js
+++ src/App.js
@@ -5,13 +5,9 @@ import Detail from "./routes/Detail";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route
-          basename={process.env.PUBLIC_URL}
-          path="/movie/:id"
-          element={<Detail />}
-        />
+        <Route path="/movie/:id" element={<Detail />} />
         <Route basename={process.env.PUBLIC_URL} path="/" element={<Home />} />
       </Routes>
     </Router>
